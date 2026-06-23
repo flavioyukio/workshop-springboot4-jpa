2045fe1853d9f4b6bf725a96510bfc3c5209e9f2
Flavio Yukio <flavio_ohphata@hotmail.com>
2026-06-23 18:24:30 -0300

    User update

--- course/src/main/java/com/educandoweb/course/resources/UserResource.java
+++ course/src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -5,8 +5,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.course.entities.User;
@@ -15,36 +20,38 @@ import com.educandoweb.course.services.UserService;
 @RestController
 public class UserResource {
 
-    @Autowired
-    private UserService service;
+	@Autowired
+	private UserService service;
 
-    @GetMapping("/users")
-    public List<User> findAll() {
-        return service.findAll();
-    }
+	@GetMapping("/users")
+	public List<User> findAll() {
+		return service.findAll();
+	}
 
-    @GetMapping("/users/{id}")
-    public ResponseEntity<User> findById(@PathVariable Long id) {
-        User obj = service.findById(id);
-        return ResponseEntity.ok().body(obj);
-    }
+	@GetMapping("/users/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
 
-    @PostMapping("/users")
-    public ResponseEntity<User> insert(@RequestBody User obj) {
-        obj = service.insert(obj);
+	@PostMapping("/users")
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
 
-        URI uri = ServletUriComponentsBuilder
-                .fromCurrentRequestUri()
-                .path("/{id}")
-                .buildAndExpand(obj.getId())
-                .toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
 
-        return ResponseEntity.created(uri).body(obj);
-    }
+		return ResponseEntity.created(uri).body(obj);
+	}
 
-    @DeleteMapping("/users/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
-        service.delete(id);
-        return ResponseEntity.noContent().build();
-    }
+	@DeleteMapping("/users/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping("/users/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+	    obj = service.update(id, obj);
+	    return ResponseEntity.ok().body(obj);
+	}
 }

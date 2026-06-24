445b6203989d1c94450b295595ddae2ec210eb4c
Flavio Yukio <flavio_ohphata@hotmail.com>
2026-06-24 12:08:48 -0300

    Exception handling

--- course/src/main/java/com/educandoweb/course/services/UserService.java
+++ course/src/main/java/com/educandoweb/course/services/UserService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.respositories.UserRepository;
+import com.educandoweb.course.services.exceptions.DataBaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,15 +32,21 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+		    repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+		    e.printStackTrace();
+		} catch (RuntimeException e) {
+		    throw new DataBaseException(e.getMessage());
+		}
 	}
-	
+
 	public User update(Long id, User obj) {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
 	}
-	
+
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
--- course/src/main/java/com/educandoweb/course/services/exceptions/ResourceExceptionHandler.java
+++ course/src/main/java/com/educandoweb/course/services/exceptions/ResourceExceptionHandler.java
@@ -20,5 +20,13 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
+	
+		@ExceptionHandler(DataBaseException.class)
+		public ResponseEntity<StandardError> database(DataBaseException e ,HttpServletRequest request ) {
+			String error = "Database error";
+			HttpStatus status = HttpStatus.NOT_FOUND;
+			StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+			return ResponseEntity.status(status).body(err);
+		}
 
 }

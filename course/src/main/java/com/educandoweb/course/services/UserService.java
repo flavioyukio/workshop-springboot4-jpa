d213cee55b67a26df9eaf7a8f599135012b562a4
Flavio Yukio <flavio_ohphata@hotmail.com>
2026-06-24 13:56:54 -0300

    Exception handling - update

--- course/src/main/java/com/educandoweb/course/services/UserService.java
+++ course/src/main/java/com/educandoweb/course/services/UserService.java
@@ -12,6 +12,8 @@ import com.educandoweb.course.respositories.UserRepository;
 import com.educandoweb.course.services.exceptions.DataBaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -33,18 +35,22 @@ public class UserService {
 
 	public void delete(Long id) {
 		try {
-		    repository.deleteById(id);
+			repository.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-		    e.printStackTrace();
+			e.printStackTrace();
 		} catch (RuntimeException e) {
-		    throw new DataBaseException(e.getMessage());
+			throw new DataBaseException(e.getMessage());
 		}
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
